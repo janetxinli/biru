ef43f3ca3ad0ad3c565aeb8f89dcc8c7b642d200
janetxinli <janetxinli@gmail.com>
2021-10-02 14:19:46 -0700

    styling changes

--- client/src/components/BeerOverview.js
+++ client/src/components/BeerOverview.js
@@ -1,24 +1,29 @@
 import React from "react";
+import Link from "next/link";
 import { Rating } from "@mui/material";
 import styles from "../styles/BeerOverview.module.scss";
 
 export default function BeerOverview({ beer }) {
   return (
-    <article className={`df df-jc-sb df-ai-c ${styles.beerOverview}`}>
-      <span className={`df df-fc ${styles.beerInfo}`}>
-        <h3>{beer.name}</h3>
-        <p>{beer.brewer}</p>
-        <p className={styles.dateAdded}>{beer.added}</p>
-        <Rating
-          name="beer-rating"
-          value={parseInt(beer.rating)}
-          readOnly
-          precision={0.5}
-          size="small"
-          className={styles.rating}
-        />
-      </span>
-      <img src="./undraw_beer.svg" className={styles.beerImage} />
+    <article className={styles.beerOverview}>
+      <Link href={`/beer/${beer.id}`}>
+        <a className="df df-jc-sb df-ai-c">
+          <span className={`df df-fc ${styles.beerInfo}`}>
+            <h3>{beer.name}</h3>
+            <p>{beer.brewer}</p>
+            <p className={styles.dateAdded}>{beer.added}</p>
+            <Rating
+              name="beer-rating"
+              value={parseFloat(beer.rating)}
+              readOnly
+              precision={0.5}
+              size="small"
+              className={styles.rating}
+            />
+          </span>
+          <img src="/undraw_beer.svg" className={styles.beerImage} />
+        </a>
+      </Link>
     </article>
   );
 }
