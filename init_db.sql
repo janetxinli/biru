5c2a4122520e79a6c8b8522d33e05fcce2382378
janetxinli <janetxinli@gmail.com>
2021-10-03 19:26:47 -0700

    add attributes and update/implement methods

--- init_db.sql
+++ init_db.sql
@@ -1,4 +1,5 @@
 CREATE TYPE serving_type AS ENUM ('draft', 'bottle', 'can');
+CREATE TYPE beer_type AS ENUM ('ale', 'lager', 'porter', 'stout', 'pilsner', 'pale ale', 'wheat', 'brown', 'blonde', 'IPA', 'sour');
 
 DROP TABLE beers;
 
@@ -8,9 +9,21 @@ CREATE TABLE beers(
   brewer VARCHAR(30) NOT NULL,
   rating FLOAT(2) NOT NULL,
   serving_type serving_type,
+  beer_type beer_type,
   abv FLOAT(2),
-  ibu INTEGER
+  ibu INTEGER,
+  date_added DATE,
+  notes VARCHAR(255)
 );
 
-INSERT INTO beers (name, brewer, rating, serving_type, abv, ibu) VALUES ('Electric Unicorn', 'Phillips', 4.5, 'can', 6.5, 75);
-INSERT INTO beers (name, brewer, rating, serving_type, abv) VALUES ('Lavender Sour', 'Moody Ales', 4.25, 'can', 4.9);
+INSERT INTO beers (name, brewer, rating, serving_type, beer_type, abv, ibu, date_added, notes)
+  VALUES ('Electric Unicorn', 'Phillips', 3.5, 'can', 'IPA', 6.5, 75, '2021-09-23', 'A light, citrusy and bitter IPA. Great for sipping on the beach or watching a game');
+
+INSERT INTO beers (name, brewer, rating, serving_type, beer_type, abv, date_added)
+  VALUES ('Lavender Sour', 'Moody Ales', 4., 'can', 'sour', 4.9, '2021-09-24');
+
+INSERT INTO beers (name, brewer, rating, serving_type, beer_type, abv, date_added, notes)
+  VALUES ('33 Acres of Ocean', '33 Acres', 4.5, 'bottle', 'pale ale', 5.3, '2021-09-20', 'This is a classic everyday beer made for glugging. Citrusy, hoppy and piney.');
+
+INSERT INTO beers (name, brewer, rating, serving_type, beer_type, abv, date_added)
+  VALUES ('No Brainer', 'Brassneck', 4.5, 'can', 'lager', 4.5, '2021-09-21');
--- routes/beer.js
+++ routes/beer.js
@@ -6,17 +6,22 @@ router.get("/", async (req, res) => {
     const beers = await pool.query("SELECT * FROM beers;");
     return res.json({ payload: beers.rows });
   } catch (err) {
-    console.log(err);
-    return res.status(500);
+    const error = error(500, err.message);
   }
 });
 
-// TODO: add 'type' field (IPA, stout, etc.), notes
-// TODO: add error handling middleware
-// TODO: add GET and PUT methods for individual beers
-
 router.post("/", async (req, res) => {
-  const { name, brewer, rating, servingType, abv, ibu } = req.body;
+  const {
+    name,
+    brewer,
+    rating,
+    serving_type,
+    beer_type,
+    abv,
+    ibu,
+    date_added,
+    notes,
+  } = req.body;
   // check that name, brewer and rating exist
   if (!name || !brewer || !rating)
     return res
@@ -25,13 +30,74 @@ router.post("/", async (req, res) => {
 
   try {
     const newBeer = await pool.query(
-      "INSERT INTO beers (name, brewer, rating, serving_type, abv, ibu) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
-      [name, brewer, rating, servingType, abv, ibu]
+      "INSERT INTO beers (name, brewer, rating, serving_type, beer_type, abv, ibu, date_added, notes) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *",
+      [
+        name,
+        brewer,
+        rating,
+        serving_type,
+        beer_type,
+        abv,
+        ibu,
+        date_added,
+        notes,
+      ]
     );
     return res.status(201).json({ payload: newBeer.rows[0] });
   } catch (err) {
     console.log(err);
     return res.status(500);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const beer = await pool.query("SELECT * FROM beers WHERE id = $1", [id]);
+    if (beer.rows.length) {
+      return res.json({ payload: beer.rows[0] });
+    }
+    return res.status(400).json({ error: true });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: true });
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const {
+    name,
+    brewer,
+    rating,
+    serving_type,
+    beer_type,
+    abv,
+    ibu,
+    date_added,
+    notes,
+  } = req.body;
+
+  try {
+    const updatedBeer = await pool.query(
+      "UPDATE beers SET name = $1, brewer = $2, rating = $3, serving_type = $4, beer_type = $5, abv = $6, ibu = $7, date_added = $8, notes = $9 where id = $10 RETURNING *",
+      [
+        name,
+        brewer,
+        rating,
+        serving_type,
+        beer_type,
+        abv,
+        ibu,
+        date_added,
+        notes,
+        id,
+      ]
+    );
+    return res.status(201).json({ payload: updatedBeer.rows[0] });
+  } catch (err) {
+    const error = error(500, err.message);
+    next(error);
   }
 });
 
