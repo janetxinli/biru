f43aade65439a36c95357e3f6150af71441b5145
janetxinli <janetxinli@gmail.com>
2021-11-11 16:45:57 -0800

    test authorization

---
+++ test/constants.js
@@ -0,0 +1,9 @@
+// authentication details
+module.exports = {
+  username: "testuser",
+  password: "somethingrandomfornow",
+  name: "Test User",
+  bio: "Beer lover",
+  token: "eyJhbGciOiJIUzI1NiJ9.MQ.B87TopbSuBBKp5qcKyC7z0OUVW_NgLAJIchD4zhQ4q8",
+  userId: 1,
+};
