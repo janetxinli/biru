1d3a892f4e006e74a6245305e16a63bbca128d39
janetxinli <janetxinli@gmail.com>
2021-12-09 11:32:03 -0800

    use auth context instead of pageProps

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,18 +2,23 @@ import React, { useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { logout } from "../services/auth";
+import { useAuth } from "../context/auth";
 import PageError from "./PageError";
 import styles from "../styles/components/Header.module.scss";
 
-const Header = ({ loggedIn }) => {
+const Header = () => {
+  const { authenticated, user, logoutUser } = useAuth();
   const router = useRouter();
 
   const [error, setError] = useState(null);
 
   const handleLogout = async (e) => {
     e.preventDefault();
+    setError(null);
+
     try {
       await logout();
+      logoutUser();
       router.push("/login");
     } catch (e) {
       setError("Unable to log out. Please try again.");
@@ -26,7 +31,7 @@ const Header = ({ loggedIn }) => {
         <h1 className={styles.logo}>
           <Link href="/">biru</Link>
         </h1>
-        {loggedIn && (
+        {authenticated && (
           <button className="btn btn-secondary" onClick={handleLogout}>
             log out
           </button>
--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Header from "./Header";
 
-const Layout = ({ loggedIn, children }) => {
+const Layout = ({ children }) => {
   return (
     <>
-      <Header loggedIn={loggedIn} />
+      <Header />
       <main className="container">{children}</main>
     </>
   );
