5af56d3d4d77072c034e3ad896bd0001c6a78851
janetxinli <janetxinli@gmail.com>
2021-12-09 11:31:06 -0800

    create HOC for authentication

---
+++ client/src/hocs/withAuth.js
@@ -0,0 +1,25 @@
+import { useRouter } from "next/router";
+import { useAuth } from "../context/auth";
+
+// redirect based on authentication status
+const withAuth = (
+  WrappedComponent,
+  redirectPath = "/login",
+  expectedStatus = true
+) => {
+  const AuthRedirectWrapper = (props) => {
+    const router = useRouter();
+    const { authenticated } = useAuth();
+
+    if (authenticated !== expectedStatus) {
+      router.push(redirectPath);
+      return <></>;
+    }
+
+    return <WrappedComponent {...props} />;
+  };
+
+  return AuthRedirectWrapper;
+};
+
+export default withAuth;
